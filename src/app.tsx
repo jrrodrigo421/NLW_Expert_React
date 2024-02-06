c7ffcd29030c6a839a588d59e72e2c32d30e6258
jrrodrigo421 <rjr89000@gmail.com>
2024-02-05 21:54:25 -0300

    feat: add and tests tailwindcss

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,9 @@
 export function App() {
-  return <h1>HELLOOOOOw teste</h1>
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 
 }
 
-export default App
