a77aafb56f43b4a6e03ceb25a8ced3d4779a865b
jrrodrigo421 <rjr89000@gmail.com>
2024-02-06 19:20:23 -0300

    feat: class 1 OK

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,16 +1,19 @@
 export function NoteCard() {
   return (
-    <div className='rounded-md bg-slate-800 p-5 space-y3 overflow-hidden relative'>
-      <span className='text-sm font-medium text-slate-300'>
-        há 2 dias
-      </span>
-      <p className='text-sm leading-6 text-slate-400'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores in optio voluptatem corrupti labore, cupiditate quibusdam placeat. Corporis a magnam earum tempora itaque! Minima ipsa itaque accusamus facere obcaecati culpa.
+    <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
+      <span className="text-sm font-medium text-slate-300">há 2 dias</span>
 
+      <p className="text-sm leading-6 text-slate-400">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus commodi
+        dolor quaerat similique asperiores minus vitae magni nemo animi iusto
+        ducimus sapiente, error placeat ratione vel. Voluptatibus sequi quis
+        ratione. Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+        commodi dolor quaerat similique asperiores minus vitae magni nemo animi
+        iusto ducimus sapiente, error placeat ratione vel. Voluptatibus sequi
+        quis ratione.
       </p>
+
       <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-
-    </div>
-  )
-
+    </button>
+  );
 }
