b4d2c346c24012be3c13916b093440d1c118e166
jrrodrigo421 <rjr89000@gmail.com>
2024-02-08 18:24:24 -0300

    feat: implementations

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,7 @@
-import * as APELIDO from '@radix-ui/react-dialog';
+import * as Dialog from '@radix-ui/react-dialog';
 import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import { X } from "lucide-react";
 
 
 interface NoteCardProps {
@@ -15,31 +16,47 @@ interface NoteCardProps {
 // aqui ele fez uma desestrututração na classe
 export function NoteCard({ note }: NoteCardProps) {
   return (
-    <APELIDO.Root>
-      <APELIDO.Trigger className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md text-left bg-slate-800 flex flex-col p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
         <span className="text-sm font-medium text-slate-300">
           {note.date.toISOString()}
         </span>
 
-        <p className="text-sm leading-6 text-slate-400">
-          {note.content}
-        </p>
+        <p className="text-sm leading-6 text-slate-400">{note.content}</p>
 
         <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-      </APELIDO.Trigger>
-      <APELIDO.Portal>
-        <APELIDO.Overlay className='inset-0 fixed bg-black/50' />
-        <APELIDO.Content className='fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none '>
-          <div className='flex flex-1 flex-col gap-3 p-5'></div>
-          <span className="text-sm font-medium text-slate-300">
-            {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
-          </span>
+      </Dialog.Trigger>
 
-          <p className="text-sm leading-6 text-slate-400">
-            {note.content}
-          </p>
-        </APELIDO.Content>
-      </APELIDO.Portal>
-    </APELIDO.Root>
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/50" />
+        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+            <X className="size-5" />
+          </Dialog.Close>
+
+          <div className="flex flex-1 flex-col gap-3 p-5">
+            <span className="text-sm font-medium text-slate-300">
+              {formatDistanceToNow(note.date, {
+                locale: ptBR,
+                addSuffix: true,
+              })}
+            </span>
+
+            <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+          </div>
+
+          <button
+            type="button"
+            className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
+          >
+            Deseja{" "}
+            <span className="text-red-400 group-hover:underline">
+              apagar essa nota
+            </span>
+            ?
+          </button>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
